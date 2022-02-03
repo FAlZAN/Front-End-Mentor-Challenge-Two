c8720dc2ec484809c6614bd414c16e8252f21537
faizan <faizanshaikh565@gmail.com>
2022-02-03 13:15:14 +0530

    Front-End Mentor Challenge Two Completed.

---
+++ script.js
@@ -0,0 +1,93 @@
+const container = document.querySelector(".container");
+const mobileCommentTemplate = document.querySelector(
+  ".mobile-comment-template"
+);
+const mobileReplyTemplate = document.querySelector(".mobile-reply-template");
+const commentField = document.querySelector(".comment-field");
+const sendBtn = document.querySelector(".send");
+
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+if (window.innerWidth < 375) {
+  console.log("this is mobile device");
+} else if (window.innerWidth > 375) {
+  console.log("this is not a mobile device");
+}
+
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    //all comments
+    const comments = data.comments;
+
+    comments.forEach((comment) => {
+      const mobileCommentTemplateClone =
+        mobileCommentTemplate.content.cloneNode(true).children[0];
+
+      let userAvatar =
+        mobileCommentTemplateClone.querySelector(".user-avatar img");
+      userAvatar.src = comment.user.image.webp;
+
+      let userName = mobileCommentTemplateClone.querySelector(".user-name");
+      userName.textContent = comment.user.username;
+
+      //you tag enable
+      if (userName.innerText === "juliusomo") {
+        userName.nextElementSibling.classList.remove("hide");
+      }
+
+      let createdAt = mobileCommentTemplateClone.querySelector(".createdAt");
+      createdAt.textContent = comment.createdAt;
+
+      let commentContent = mobileCommentTemplateClone.querySelector(".comment");
+      commentContent.textContent = comment.content;
+
+      let score = mobileCommentTemplateClone.querySelector(".score-count");
+      score.textContent = comment.score;
+
+      container.appendChild(mobileCommentTemplateClone);
+
+      // Replies on existing comment
+      const replies = comment.replies;
+      replies.forEach((reply) => {
+        const mobileReplyTemplateClone =
+          mobileReplyTemplate.content.cloneNode(true).children[0];
+
+        let userAvatar =
+          mobileReplyTemplateClone.querySelector(".user-avatar img");
+        userAvatar.src = reply.user.image.webp;
+
+        let userName = mobileReplyTemplateClone.querySelector(".user-name");
+        userName.textContent = reply.user.username;
+
+        //you tag enable
+        if (userName.innerText === "juliusomo") {
+          //mobile section
+          userName.parentNode.parentNode.lastElementChild.lastElementChild.remove();
+          userName.nextElementSibling.classList.remove("hide");
+          userName.parentNode.parentNode.lastElementChild.firstElementChild.nextElementSibling.classList.remove(
+            "hide"
+          );
+
+          //desktop section
+          userName.parentNode.lastElementChild.remove();
+          console.log(
+            userName.parentNode.childNodes[9].classList.remove("hide")
+          );
+        }
+
+        let createdAt = mobileReplyTemplateClone.querySelector(".createdAt");
+        createdAt.textContent = reply.createdAt;
+
+        let commentContent = mobileReplyTemplateClone.querySelector(".comment");
+        commentContent.textContent = reply.content;
+
+        let score = mobileReplyTemplateClone.querySelector(".score-count");
+        score.textContent = reply.score;
+
+        container.appendChild(mobileReplyTemplateClone);
+      });
+    });
+  });
